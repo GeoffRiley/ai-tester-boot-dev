871dcc6666c8566268d59b586ea2c254c9468fe5
Geoff Riley <geoffr@adaso.com>
2026-01-05 19:44:14 +0000

    Command line interface, part 1?

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
-    #print("Hello from ai-tester!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    prompt = args.user_prompt
+    
     resp = client.models.generate_content(model="gemini-2.5-flash",
                                           contents=prompt)
     if resp is None or resp.usage_metadata is None:
         raise Excption("AI client failed to respond")
+
     print(f"User prompt: {prompt}")
     print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
