23ad24fa9d01084177567bd9fbe3ae921ad43ded
Geoff Riley <geoffr@adaso.com>
2026-01-05 23:58:02 +0000

    A wee fix in the exception circuit.

--- functions/put_file_content.py
+++ functions/put_file_content.py
@@ -20,5 +20,5 @@ def write_file(working_directory, file_path, content):
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
 
     except Exception as e:
-        print(f"Error: {e}")
+        return f"Error: {e}"
 
